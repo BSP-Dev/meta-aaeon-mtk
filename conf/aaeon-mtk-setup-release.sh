a7a302b574acb64bc7bfe6203b3c0e2c270852c0
FoxYang12 <foxyang@aaeon.com.tw>
2025-10-14 17:35:16 +0800

    @MODEL:[uCOM-M510] @NEW: 1.modify setup script @BUG: 1. @EFFECT:

--- conf/aaeon-mtk-setup-release.sh
+++ conf/aaeon-mtk-setup-release.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sed -i '32a\ \ ##OEROOT##/../meta-aaeon-mtk \\' src/meta-rity/meta/conf/templates/default/bblayers.conf.sample
+sed -i '28a\ \ ##OEROOT##/../meta-aaeon-mtk \\' src/meta-rity/meta/conf/templates/default/bblayers.conf.sample
 
 sed -i 's/#MACHINE ??= "genio-510-evk"/MACHINE ??= "genio-510-evk"/' src/meta-rity/meta/conf/templates/default/local.conf.sample
 sed -i 's/MACHINE ??= "genio-700-evk"/#MACHINE ??= "genio-700-evk"/' src/meta-rity/meta/conf/templates/default/local.conf.sample
 sed -i '140a\EXTRA_IMAGE_FEATURES:append = " tools-sdk"\' src/meta-rity/meta/conf/templates/default/local.conf.sample
 
-sed -i '/#IMAGE_INSTALL:append:i300a = "lib32-mtk-vpud"/a \
+sed -i '/#DEFAULTTUNE:virtclass-multilib-lib32 = "armv7athf-neon"/a \
 # AAEON: Alan Modified: 24.08.27\
 ###########################################################################\
 PACKAGE_CLASSES = "package_deb"\
